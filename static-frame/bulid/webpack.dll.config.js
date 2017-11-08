64ebdba32fac95402898452f18ae34460d68416e
laihuamin <1445509994@qq.com>
2017-11-08 10:26:17 +0800

    webpack.dll.config.js

---
+++ static-frame/bulid/webpack.dll.config.js
@@ -0,0 +1,28 @@
+/**
+ * lip.fan
+ * 预编译
+ */
+const webpack = require('webpack')
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, '../dist/assets/dll'),
+        filename: '[name].dll-[hash].js',
+        library: '[name]'
+    },
+    entry: {
+        vendor: ['xtemplate', 'es6-promise', '@base/lazyload', '@base/env']
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new CleanWebpackPlugin(['assets/dll'], {
+            root: path.resolve('./dist')
+        }),
+        new webpack.DllPlugin({
+            path: path.join(__dirname, '../dist/assets', '[name]-manifest.json'),
+            name: '[name]'
+        })
+    ]
+}
